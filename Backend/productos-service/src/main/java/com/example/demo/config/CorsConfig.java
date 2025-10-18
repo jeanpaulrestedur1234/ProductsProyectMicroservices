b61e6589f9feacc7268037a7071169c93918de6a
jean paul <jean2paul.acostasuarez@gmail.com>
2025-10-18 05:43:24 +0000

    feat: Update CORS configuration to allow all origins for inventory and product services

--- Backend/productos-service/src/main/java/com/example/demo/config/CorsConfig.java
+++ Backend/productos-service/src/main/java/com/example/demo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200"); // Angular
+        config.addAllowedOriginPattern("*"); // Angular
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
